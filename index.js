b0654184e85f8053af5820acefd6c4d325eef3bc
Irfansyah001 <syahirfan545@gmail.com>
2025-10-28 09:59:28 +0700

    refactor: enhance comments for clarity in index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const app = express();
-const db = require('./models');
-const PORT = 3307
+const express = require('express'); // Import Express framework
+const app = express(); // Create an Express application
+const db = require('./models'); // Import Sequelize models
+const PORT = 3307 // Define the port number
 
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON request bodies
 
-app.use(express.urlencoded({ 
-    extended: true 
+app.use(express.urlencoded({ // Middleware to parse URL-encoded request bodies
+    extended: true // Allow nested objects
 }));
 
-app.listen(PORT, async () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, async () => { // Start the server
+    console.log(`Server is running on http://localhost:${PORT}`); // Log the server URL
 });
 
-db.sequelize.sync()
-.then((result) => {
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+db.sequelize.sync() // Synchronize Sequelize models with the database
+.then((result) => { // On successful synchronization
+    app.listen(PORT, () => { // Start the server
+        console.log(`Server is running on http://localhost:${PORT}`); // Log the server URL
     });
 })
 
