e0c57f1b8d89848007649e50656a44c8f4694a01
Irfansyah001 <syahirfan545@gmail.com>
2025-11-25 22:17:31 +0700

    feat: implement createKomik function for adding new komik entries

---
+++ controllers/komikController.js
@@ -0,0 +1,19 @@
+const db = require("../models");
+const komikServices = require("../services/komikServices");
+
+async function createKomik(req, res) {
+    try {
+        const komikData = req.body;
+
+        if (req.file) {
+            komikData.imageType = req.file.mimetype;
+            komikData.imageName = req.file.originalname;
+            komikData.imageData = req.file.buffer;
+        }
+
+        const result = await komikServices.createKomik(db, komikData);
+        res.status(201).json({ success: true, data: result });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
